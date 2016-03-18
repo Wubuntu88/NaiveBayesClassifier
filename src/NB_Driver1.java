5c4b1776125eb54418d8891281530a92144e7404
Will <wgilles1@emich.edu>
2016-03-17 23:17:32 -0400

    I added this driver for part two of the naive bayes assignment.

---
+++ src/NB_Driver1.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+
+public class NB_Driver1 {
+
+	public static void main(String[] args) {
+
+		String trainingFileName = "part1/myTrain1";
+		String testFileName = "part1/test1";
+		String outputFileName = "part1/output1";
+		
+		NaiveBayesClassifier nb = null;
+		NaiveBayesIO nbIO = new NaiveBayesIO();
+		
+		try {
+			nb = nbIO.instantiateNBClassifierWithTrainingData(trainingFileName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		/* a) Classification of records in test file (stored in part1/output1)*/
+		ArrayList<Record> recordsToClassify = null;
+		try {
+			recordsToClassify = nbIO.readRecordsFromFile(testFileName);
+		} catch (Exception e) {
+			System.out.println("error in loading test data");
+			e.printStackTrace();
+		}
+		ArrayList<ClassificationInfo<Integer>> classificationInfos = nb.classifyRecords(recordsToClassify);
+		nbIO.writeRecordsToFile(outputFileName, recordsToClassify, classificationInfos);
+		
+		/* b) training error on the data */
+		double trainingError = nb.calculateTrainingError();
+		System.out.println("training error: " + trainingError);
+		/* c) one out error on the data */
+		double oneOutError = nb.calculateOneOutError();
+		System.out.println("one out error: " + oneOutError);
+		
+		/* d) probability tables */
+		System.out.println(nbIO.probabilityMatrixStringInHumanReadableForm(nb.probMatrixCopy()));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
